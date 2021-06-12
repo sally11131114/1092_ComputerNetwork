11aeb1d6926d5b43a38fc7e2fabbe5c193c0619c
sally11131114 <sally11131114@gmail.com>
2021-06-13 01:05:50 +0800

    step1-MATH DNS

---
+++ server.cpp
@@ -0,0 +1,233 @@
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <iostream>
+#include <ctime>
+#include <cmath>
+#include <fstream>
+using namespace std;
+
+#define SRVPORT "2500"
+#define MSS 1000
+
+typedef struct TCP_segment{
+    unsigned int seq;
+    unsigned int ack;
+    bool URG, ACK, PSH, RST, SYN, FIN;
+    char data[MSS];
+}segment;
+
+void find(int sockfd, segment recv);
+void math(int sockfd, segment recv);
+void dns(int sockfd, segment recv);
+void file(int sockfd, segment recv);
+struct addrinfo cliinfo;
+
+int main(){
+    int sockfd, rv, numbyte;
+    struct addrinfo temp,  *srvinfo;
+    socklen_t cli_addrlen;
+    segment send, recv;
+    srand(time(NULL));
+
+    memset(&temp, 0, sizeof(temp));
+    temp.ai_family = AF_INET;
+    temp.ai_socktype = SOCK_DGRAM;
+    temp.ai_flags = AI_PASSIVE;
+
+    if(getaddrinfo(NULL, SRVPORT, &temp, &srvinfo)){
+        perror("getaddrinfo");
+        exit(1);
+    }
+    if((sockfd = socket(srvinfo->ai_family, srvinfo->ai_socktype, srvinfo->ai_protocol)) == -1){
+        perror("Server socket");
+        exit(1);
+    }
+    if(bind(sockfd, srvinfo->ai_addr, srvinfo->ai_addrlen)==-1){
+        close(sockfd);
+        perror("Server bind");
+        exit(1);
+    }
+
+    cout << "=====Start the three-way handshake=====" << endl;
+    freeaddrinfo(srvinfo);
+    memset(&cliinfo, 0, sizeof(cliinfo));
+    memset(&recv, 0, sizeof(recv));
+    if((numbyte = recvfrom(sockfd, &recv, sizeof(recv), 0, (struct sockaddr *)&cliinfo, &cli_addrlen) == -1)){
+        perror("Server recvfrom");
+        exit(1);
+    }
+    cout << "Receive a packet(SYN) from client" << endl;
+    cout << "         Receive a packet (seq_num = " << recv.seq << ")" << endl;
+    cout << "Send a packet(SYN/ACK) to client " << endl;
+    memset(&send, 0, sizeof(send));
+    send.SYN=1;
+    send.seq=rand()%10000;
+    send.ack=recv.seq+1;
+    if((numbyte = sendto(sockfd, &send, sizeof(send), 0, (struct sockaddr *)&cliinfo, sizeof(cliinfo))) == -1 ){
+        perror("Server sendto");
+        exit(1);
+    }
+    memset(&recv, 0, sizeof(recv));
+    if((numbyte = recvfrom(sockfd, &recv, sizeof(recv), 0, (struct sockaddr *)&cliinfo, &cli_addrlen) == -1)){
+        perror("Server recvfrom");
+        exit(1);
+    }
+    cout << "Receive a packet(ACK) from client" << endl;
+    cout << "         Receive a packet (seq_num = " << recv.seq << ", ack_num = " << recv.ack << ")" << endl;
+    cout << "=====Complete the three-way handshake=====" << endl << endl;
+
+    while(1){
+        memset(&recv, 0, sizeof(recv));
+        if((numbyte = recvfrom(sockfd, &recv, sizeof(recv), 0, (struct sockaddr *)&cliinfo, &cli_addrlen) == -1)){
+            perror("Server recvfrom");
+            exit(1);
+        }
+        cout << "Receive a packet(Request) from client" << endl;
+        find(sockfd, recv);
+    }
+}
+void find(int sockfd, segment recv){
+    char buf[1000];
+    string buf_tmp;
+    const char s[2] = " ";
+    strcpy(buf, recv.data);
+    buf_tmp=strtok(buf, s);
+    if(buf_tmp=="MATH") 
+        math(sockfd, recv);
+    else if(buf_tmp=="DNS")
+        dns(sockfd,recv);
+    else if(buf_tmp=="FILE")
+        file(sockfd,recv);
+    else
+        cout << "find error" << endl;
+}
+void math(int sockfd, segment recv){
+    char buf[1000];
+    string buf_tmp, a, b, ans;
+    int numbyte;
+    const char s[2] = " ";
+    strcpy(buf, recv.data);
+    buf_tmp=strtok(buf, s); //buf_tmp = MATH
+    buf_tmp=strtok(NULL, s); //buf_tmp= function
+
+    if(buf_tmp=="ADD"){
+        a=strtok(NULL, s);
+        b=strtok(NULL, s);
+        ans="The answer of "+buf_tmp+" "+a+" "+b+" is "+to_string(stof(a)+stof(b));
+    }
+    else if(buf_tmp=="MINUS"){
+        a=strtok(NULL, s);
+        b=strtok(NULL, s);
+        ans="The answer of "+buf_tmp+" "+a+" "+b+" is "+to_string(stof(a)-stof(b));
+    }
+    else if(buf_tmp=="DIVIDE"){
+        a=strtok(NULL, s);
+        b=strtok(NULL, s);
+        ans="The answer of "+buf_tmp+" "+a+" "+b+" is "+to_string(stof(a)/stof(b));
+    }
+    else if(buf_tmp=="MUTIPLE"){
+        a=strtok(NULL, s);
+        b=strtok(NULL, s);
+        ans="The answer of "+buf_tmp+" "+a+" "+b+" is "+to_string(stof(a)*stof(b));
+    }
+    else if(buf_tmp=="SQUARE_ROOT"){
+        a=strtok(NULL, s);
+        ans="The answer of "+buf_tmp+" "+a+" is "+to_string(sqrt(stof(a)));
+    }
+    else if(buf_tmp=="POWER"){
+        a=strtok(NULL, s);
+        b=strtok(NULL, s);
+        ans="The answer of "+buf_tmp+" "+a+" "+b+" is "+to_string(pow(stof(a), stof(b)));
+    }
+    cout << "Send a packet(Request MATH) to client " << endl << endl;
+    segment send;
+    memset(&send, 0, sizeof(send));
+    strcpy(send.data, ans.c_str());
+    if((numbyte = sendto(sockfd, &send, sizeof(send), 0, (struct sockaddr *)&cliinfo, sizeof(cliinfo))) == -1 ){
+        perror("Server request(MATH) sendto");
+        exit(1);
+    }
+}
+void dns(int sockfd, segment recv){
+    char buf[1000];
+    string buf_tmp, ans_tmp;
+    char ans[INET6_ADDRSTRLEN];
+    int numbyte;
+    const char s[2] = " ";
+    strcpy(buf, recv.data);
+    buf_tmp=strtok(buf, s); //buf_tmp = DNS
+    buf_tmp=strtok(NULL, s); //buf_tmp= "google.com"
+
+    struct addrinfo temp, *res;
+    memset(&temp, 0, sizeof(temp));
+    temp.ai_family=AF_INET;
+    temp.ai_socktype=SOCK_STREAM;
+
+    if(getaddrinfo(buf_tmp.c_str(), NULL, &temp, &res)){
+        perror("DNS getaddrinfo");
+        exit(1);
+    }
+    struct sockaddr_in *ipv4 = (struct sockaddr_in *)res->ai_addr;
+    inet_ntop(res->ai_family, &(ipv4->sin_addr), ans, sizeof ans);
+    ans_tmp="The ipv4 of "+buf_tmp+" is "+ans;
+    cout << "Send a packet(Request DNS) to client " << endl << endl;
+    segment send;
+    memset(&send, 0, sizeof(send));
+    strcpy(send.data, ans_tmp.c_str());
+    if((numbyte = sendto(sockfd, &send, sizeof(send), 0, (struct sockaddr *)&cliinfo, sizeof(cliinfo))) == -1 ){
+        perror("Server request(DNS) sendto");
+        exit(1);
+    }
+}
+void file(int sockfd, segment recv){
+    char buf[1000];
+    string buf_tmp, filename;
+    segment send;
+    int numbyte;
+    const char s[2] = " ";
+    strcpy(buf, recv.data);
+    buf_tmp=strtok(buf, s); //buf_tmp = FILE
+    filename=strtok(NULL, s); //buf_tmp= 1.mp4
+    //filesize
+    ifstream f(filename, ios::binary);
+    f.seekg(0, ios::end);
+    int size = f.tellg();
+    //send filesize
+    memset(&send, 0, sizeof(send));
+    send.seq=recv.ack;
+    send.ack=size;
+    if((numbyte = sendto(sockfd, &send, sizeof(send), 0, (struct sockaddr *)&cliinfo, sizeof(cliinfo))) == -1 ){
+        perror("Server request sendto");
+        exit(1);
+    }
+    //sendfile
+    int file = open(filename.c_str())
+    while(1){
+        memset(&recv, 0, sizeof(recv));
+        if((numbyte = recvfrom(sockfd, &recv, sizeof(recv), 0, (struct sockaddr *)&cliinfo, &cli_addrlen) == -1)){
+            perror("Server recvfrom");
+            exit(1);
+        }
+        if(recv.FIN == 1) break;
+        lseek(file, recv.ack, SEEK_SET);
+        recv.ack+=send.seq;
+        memset(&send, 0, sizeof(send));
+        send.seq=recv.ack;
+        send.ack=recv.seq+1;
+        read(file, send.data, MSS);
+        if((numbyte = sendto(sockfd, &send, sizeof(send), 0, (struct sockaddr *)&cliinfo, sizeof(cliinfo))) == -1 ){
+            perror("Server request sendto");
+            exit(1);
+        }
+    }
+                                
+}
